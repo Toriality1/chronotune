64b748069a7eb515433d59baa929bc84b77aff0c
Pedro Crelier <torialitym@gmail.com>
2025-08-28 15:35:03 -0300

    Add logo and favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import NumberOfSongsButton from "../components/NumberOfSongsButton";
+import Image from "next/image";
 
 export default function Page() {
   return (
     <div className="flex flex-col h-screen justify-center text-center">
+      <Image src="/favicon.ico" width={80} height={80} alt="logo" className="mx-auto mb-3" />
       <h1 className="text-4xl font-bold">Chronotune</h1>
       <p className="mb-4">Guess the release date of random songs!</p>
       <NumberOfSongsButton />
---
+++ todo.md
@@ -0,0 +1,1 @@
+- Logo is temporary, improve it!
